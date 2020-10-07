14403bd3f0d4d596531affcc598c080c76fbe798
Max1330602 <1330602@cstj.qc.ca>
2020-10-07 11:06:25 -0400

    commit initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using App_Brycol.Vues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App_Brycol
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreerPiece_Click(object sender, RoutedEventArgs e)
+        {
+            DimensionsPiece popUp = new DimensionsPiece();
+            popUp.ShowDialog();
+        }
+
+        private void btnTeleverserPiece_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Option de téléversement à venir!");
+        }
+    }
+}
---
+++ Vues/DimensionsPiece.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_Brycol.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour DimensionsPiece.xaml
+    /// </summary>
+    public partial class DimensionsPiece : Window
+    {
+        public DimensionsPiece()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContinuer_Click(object sender, RoutedEventArgs e)
+        {
+            Grid gridMW = (Grid)Application.Current.MainWindow.FindName("gridMainWindow");
+            ContentPresenter cpMW = (ContentPresenter)Application.Current.MainWindow.FindName("presenteurContenu");
+            this.Close();
+            gridMW.Children.Clear();
+            gridMW.Children.Add(cpMW);
+            cpMW.Content = new PlanDeTravail();
+        }
+
+        private void txtLargeur_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtLargeur.Text = Regex.Replace(txtLargeur.Text, "[^0-9]+", "");
+        }
+
+        private void txtLongueur_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtLongueur.Text = Regex.Replace(txtLongueur.Text, "[^0-9]+", "");
+
+        }
+
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
