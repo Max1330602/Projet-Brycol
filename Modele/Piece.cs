b372c06321d33b95d2ecbdc28146d2120b599440
jacobdesrosiers <jacobdesrosiers95@gmail.com>
2020-10-14 09:36:05 -0400

    modèle de Pièce

---
+++ Modele/Piece.cs
@@ -0,0 +1,25 @@
+﻿using App_Brycol.Outils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Brycol.Modele
+{
+
+    class Piece
+    {
+        public int ID { get; set; }
+        public int IdProjet { get; set; }
+        public int IdTypeDePiece { get; set; }
+        public float Largeur { get; set; }
+        public float Longueur { get; set; }
+        public string Nom { get; set; }
+
+        public Piece() 
+        {
+
+        }
+    }
+}
