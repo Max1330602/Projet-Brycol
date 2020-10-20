08f06e98bc608f1cfbc45af0e370f4c61d2ef26f
BouleDeGommme <yannick.auger@hotmail.com>
2020-10-20 14:10:56 -0400

    modele plan

---
+++ Modele/Plan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Brycol.Modele
+{
+    class Plan
+    {
+        public int ID { get; set; }
+        public Piece Piece { get; set; }
+        public bool est3D { get; set; }
+        public float tailleZoom { get; set; }
+
+        public Plan()
+        {
+
+        }
+    }
+}
